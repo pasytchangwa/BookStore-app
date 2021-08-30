70c3b4d86b7a8c03d3da825ea3cfd9cf9eda05bc
Sylvestre <pasytchangwa@gmail.com>
2021-08-30 21:08:05 +0300

    Create BooksSetup component

---
+++ src/components/books/BooksSetup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BooksSetup = () => (
+  <div className="changes">
+    <button type="button">Comments |</button>
+    <button type="button">Remove |</button>
+    <button type="button">Edit</button>
+  </div>
+);
+
+export default BooksSetup;
