cae6a3dfe03ce68a13de9cb999c38aacfde9d920
Sylvestre <pasytchangwa@gmail.com>
2021-08-31 13:18:06 +0300

    Update the book container with useSelector

--- src/components/books/BooksContainer.js
+++ src/components/books/BooksContainer.js
@@ -1,49 +1,16 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 import BooksList from './BooksList';
-import InputHeader from './InputHeader';
 import InputBooks from './InputBooks';
 import '../CSS/BooksContainer.css';
 
-const books = [
-  {
-    id: 1,
-    category: 'Action',
-    title: 'The hunger Games',
-    author: 'Suxanne Collins',
-    progress: {
-      currentChapter: 'Chapter 17',
-      completed: '64',
-    },
-  },
-  {
-    id: 2,
-    category: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    progress: {
-      currentChapter: 'Chapter 3: "A Lesson Learned"',
-      completed: '8',
-    },
-  },
-  {
-    id: 3,
-    category: 'Economy',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    progress: {
-      currentChapter: 'Introduction',
-      completed: '0',
-    },
-  },
-];
-
-const BooksContainer = () => (
-  <main>
-    <BooksList books={books} />
-    <div className="line" />
-    <InputHeader />
-    <InputBooks />
-  </main>
-);
+const BooksContainer = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <main>
+      <BooksList books={books} />
+      <InputBooks />
+    </main>
+  );
+};
 
 export default BooksContainer;
