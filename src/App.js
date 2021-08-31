7dd22a55f080684c6bf15cdc2f35d568afc6941b
Sylvestre <pasytchangwa@gmail.com>
2021-08-31 13:48:21 +0300

    Import provider and store and update app.js component file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { provider } from 'react-redux';
+import store from './redux/configureStore';
 import Nav from './components/Nav';
 
 const App = () => (
-  <Nav />
+  <provider store={store}>
+    <Nav />
+  </provider>
 );
 
 export default App;
