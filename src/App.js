0a48a2014ee796d2aefc833e886c6bb61295df3f
Sylvestre <pasytchangwa@gmail.com>
2021-08-31 13:58:07 +0300

    Fix Provider typo error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import Nav from './components/Nav';
 
 const App = () => (
-  <provider store={store}>
+  <Provider store={store}>
     <Nav />
-  </provider>
+  </Provider>
 );
 
 export default App;
