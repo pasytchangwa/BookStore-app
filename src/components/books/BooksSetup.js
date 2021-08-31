ffde96bbbafaaac7ef3763e29998a7aa29e14ba9
Sylvestre <pasytchangwa@gmail.com>
2021-08-31 13:45:57 +0300

    Add remove instance

--- src/components/books/BooksSetup.js
+++ src/components/books/BooksSetup.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import PropTypes from 'propTypes';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
-const BooksSetup = () => {
+const BooksSetup = ({ id }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => dispatch(removeBook(id));
+
   const display = {
     display: 'flex',
     gap: '0.5rem',
   };
+
   return (
     <div style={display}>
       <button type="button">Comments |</button>
-      <button type="button">Remove |</button>
+      <button
+        type="button"
+        onClick={handleRemove}
+      >
+        Remove |
+      </button>
       <button type="button">Edit</button>
     </div>
   );
+};
+
+BooksSetup.propTypes = {
+  id: PropTypes.string.isRequired,
 };
 
 export default BooksSetup;
