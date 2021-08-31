7a66ba436783647ee5e0158d2c430c1c0a248ece
Sylvestre <pasytchangwa@gmail.com>
2021-08-31 12:18:08 +0300

    Write reducer and exploit it as default

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,8 +3,34 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
-export const addBook = payload => ({
-    type: ADD_BOOK,
-    payload
-})
-export const removeBook = ...
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          ...action.payload,
+          progress: {
+            currentChapter: 'NEW CHAPTER',
+            completed: '0',
+          },
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
